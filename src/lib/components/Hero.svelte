24b3449cc3c800367cdce1ab754d9d3ba11c457d
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2024-08-15 01:41:26 +0300

    feat: first attempt of a hero section

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,12 @@
+<div class="hero bg-base-200 min-h-screen">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<div class="relative">
+			<img src="/photos/2-birds.jpeg" class="h-[80vh] aspect-square shadow-2xl" alt="hero" />
+			<!-- <div class="absolute inset-0 bg-gradient-to-t from-base-200 to-transparent to-30%"></div> -->
+		</div>
+		<div>
+			<h1 class="text-5xl font-bold">Sedat Dalgıç</h1>
+			<p class="py-6">Taklacı güvercin yetiştiricisiyim. 2015 yılından beri bu işi yapıyorum.</p>
+		</div>
+	</div>
+</div>
